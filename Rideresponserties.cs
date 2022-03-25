dfd21ffeec852070f01f4682ba4b5e5a63d53c4e
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-03-25 19:27:39 +0530

    Add files via upload

---
+++ Rideresponserties.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class RideRepository
+    {
+        Dictionary<string, List<Ride>> userRides = null;
+
+        public RideRepository()
+        {
+            this.userRides = new Dictionary<string, List<Ride>>();
+        }
+        public void AddRide(string userId, Ride[] rides)
+        {
+            bool rideList = this.userRides.ContainsKey(userId);
+            try
+            {
+                if (!rideList)
+                {
+                    List<Ride> list = new List<Ride>();
+                    list.AddRange(rides);
+                    this.userRides.Add(userId, list);
+                }
+            }
+            catch (CabInvoiceException)
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_USER_ID, "Invalid UserId");
+            }
+        }
+        public Ride[] GetRides(string userId)
+        {
+            try
+            {
+                return this.userRides[userId].ToArray();
+            }
+            catch
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_USER_ID, "Invalid User ID");
+            }
+        }
+    }
+}
